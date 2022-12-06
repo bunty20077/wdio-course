8bf575eb81333974fc825cb6de0ef7a330bd111d
bunty20077 <sayantan20077@rediffmail.com>
2022-12-06 11:02:57 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+Create a new npm project 
+=========================
+mkdir <projectname>
+cd <projectname>
+
+Install Webdriver io CLI
+=========================
+npm install @wdio/cli
+
+Setup Config
+=============
+npx wdio config
+
+
+Webdriver Command List
+=======================
+http://v4.webdriver.io/api/protocol/touchDown.html
+https://v6.webdriver.io/docs/selectors.html
---
+++ test/specs/home.js
@@ -0,0 +1,33 @@
+//https://practice.automationbro.com/
+
+describe('Home',() => {
+    it('Open URL and assert Title', async ()=> {
+        await browser.url('https://practice.automationbro.com/');
+        await expect(browser).toHaveTitle('Practice E-Commerce Site – Automation Bro');
+    });
+
+    it('Open About US URL and assert Url', async ()=> {
+        await browser.url('https://practice.automationbro.com/about');
+        await expect(browser).toHaveUrl('https://practice.automationbro.com/about/');
+    });
+
+    it('Click on get Start button and assert Url contains get-started', async ()=> {
+        await browser.url('https://practice.automationbro.com');
+        await $('#get-started').click();
+        await expect(browser).toHaveUrlContaining('get-started');
+    });
+
+
+    it('Click on get Logo Image and assert Url doesnot contains get-started', async ()=> {
+        await browser.url('https://practice.automationbro.com');
+        await $('//img[@alt="Practice E-Commerce Site"]').click();
+        await expect(browser).not.toHaveUrlContaining('get-started');
+    });
+
+
+    it('Find Head Element and assert the text', async ()=> {
+        await browser.url('https://practice.automationbro.com');
+        const headingText =  await $('.elementor-widget-container > h1').getText();
+        await expect(headingText).toEqual('Think different. Make different.');
+    });
+});
